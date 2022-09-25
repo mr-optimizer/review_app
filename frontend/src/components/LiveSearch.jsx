61c5ddd85546b4d8a822393c92cf3d47c4bbd951
Shubham Kumar <sktovisit@gmail.com>
2022-09-25 16:26:02 +0530

    handling form state

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -5,6 +5,7 @@ export default function LiveSearch({
   value = "",
   placeholder = "",
   results = [],
+  name,
   resultContainerStyle,
   selectedResultStyle,
   inputStyle,
@@ -65,6 +66,8 @@ export default function LiveSearch({
     >
       <input
         type="text"
+        id={name}
+        name={name}
         className={getInputStyle()}
         placeholder={placeholder}
         onFocus={handleOnFocus}
@@ -116,7 +119,7 @@ const SearchResults = ({
   if (!visible) return null;
 
   return (
-    <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scroll-bar">
+    <div className="absolute z-50 right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scroll-bar">
       {results.map((result, index) => {
         const getSelectedClass = () => {
           return selectedResultStyle
@@ -125,7 +128,7 @@ const SearchResults = ({
         };
         return (
           <ResultCard
-            key={result.id}
+            key={index.toString()}
             item={result}
             renderItem={renderItem}
             resultContainerStyle={resultContainerStyle}
--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNotification } from "../../hooks";
 import { commonInputClasses } from "../../utils/theme";
+import Submit from "../form/Submit";
 import LiveSearch from "../LiveSearch";
+import ModalContainer from "../models/ModalContainer";
+import WritersModal from "../models/WritersModal";
 import TagsInput from "../TagsInput";
 
 export const results = [
@@ -42,9 +46,30 @@ export const results = [
   },
 ];
 
+const defaultMovieInfo = {
+  title: "",
+  storyLine: "",
+  tags: [],
+  cast: [],
+  director: {},
+  writers: [],
+  releseDate: "",
+  poster: null,
+  genres: [],
+  type: "",
+  language: "",
+  status: "",
+};
+
 export default function MovieForm() {
+  const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
+  const [showWritersModal, setShowWritersModal] = useState(false);
+
+  const { updateNotification } = useNotification();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(movieInfo);
   };
 
   const renderItem = (result) => {
@@ -56,42 +81,132 @@ export default function MovieForm() {
     );
   };
 
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+
+    setMovieInfo({ ...movieInfo, [name]: value });
+  };
+
+  const updateTags = (tags) => {
+    setMovieInfo({ ...movieInfo, tags });
+  };
+
+  const updateDirector = (profile) => {
+    setMovieInfo({ ...movieInfo, director: profile });
+  };
+
+  const updateWriters = (profile) => {
+    const { writers } = movieInfo;
+    for (let writer of writers) {
+      if (writer.id === profile.id) {
+        return updateNotification(
+          "warning",
+          "This profile is already selected!"
+        );
+      }
+    }
+    setMovieInfo({ ...movieInfo, writers: [...writers, profile] });
+  };
+
+  const hideWritersModal = () => {
+    setShowWritersModal(false);
+  };
+
+  const displayWritersModal = () => {
+    setShowWritersModal(true);
+  };
+
+  const handleWriterRemove = (profileId) => {
+    const { writers } = movieInfo;
+    const newWriters = writers.filter(({ id }) => id !== profileId);
+    if (!newWriters.length) hideWritersModal();
+    setMovieInfo({ ...movieInfo, writers: [...newWriters] });
+  };
+
+  const { title, storyLine, director, writers } = movieInfo;
+
   return (
-    <form onSubmit={handleSubmit} className="flex space-x-3">
-      <div className="w-[70%] space-y-5">
-        <div>
-          <Label htmlFor="title">Title</Label>
-          <input
-            id="title"
-            type="text"
-            className={commonInputClasses + " border-b-2 font-semibold text-xl"}
-            placeholder="Titanic"
-          />
-        </div>
+    <>
+      <form onSubmit={handleSubmit} className="flex space-x-3">
+        <div className="w-[70%] space-y-5">
+          <div>
+            <Label htmlFor="title">Title</Label>
+            <input
+              id="title"
+              value={title}
+              onChange={handleChange}
+              name="title"
+              type="text"
+              className={
+                commonInputClasses + " border-b-2 font-semibold text-xl"
+              }
+              placeholder="Titanic"
+            />
+          </div>
 
-        <div>
-          <Label htmlFor="storyLine">Story line</Label>
-          <textarea
-            id="storyLine"
-            className={commonInputClasses + " border-b-2 resize-none h-24"}
-            placeholder="Movie storyline..."
-          ></textarea>
-        </div>
+          <div>
+            <Label htmlFor="storyLine">Story line</Label>
+            <textarea
+              value={storyLine}
+              onChange={handleChange}
+              name="storyLine"
+              id="storyLine"
+              className={commonInputClasses + " border-b-2 resize-none h-24"}
+              placeholder="Movie storyline..."
+            ></textarea>
+          </div>
 
-        <div>
-          <Label htmlFor="tags">Tags</Label>
-          <TagsInput />
-        </div>
+          <div>
+            <Label htmlFor="tags">Tags</Label>
+            <TagsInput name="tags" onChange={updateTags} />
+          </div>
 
-        <LiveSearch
-          results={results}
-          placeholder="Search profile"
-          renderItem={renderItem}
-          onSelect={(result) => console.log(result)}
-        />
-      </div>
-      <div className="w-[30%] h-5 bg-blue-400"></div>
-    </form>
+          <div className="">
+            <Label htmlFor="director">Director</Label>
+            <LiveSearch
+              name="director"
+              results={results}
+              placeholder="Search profile"
+              renderItem={renderItem}
+              onSelect={updateDirector}
+              value={director.name}
+            />
+          </div>
+
+          <div className="">
+            <div className="flex justify-between">
+              <LabelWithBadge badge={writers.length} htmlFor="writers">
+                Writers
+              </LabelWithBadge>
+              <button
+                onClick={displayWritersModal}
+                className="dark:text-white text-primary hover:underline transition"
+              >
+                View All
+              </button>
+            </div>
+            <LiveSearch
+              name="writers"
+              results={results}
+              placeholder="Search profile"
+              renderItem={renderItem}
+              onSelect={updateWriters}
+              value={director.name}
+            />
+          </div>
+
+          <Submit value="Upload" />
+        </div>
+        <div className="w-[30%] h-5 bg-blue-400"></div>
+      </form>
+
+      <WritersModal
+        onClose={hideWritersModal}
+        visible={showWritersModal}
+        profiles={writers}
+        onRemoveClick={handleWriterRemove}
+      />
+    </>
   );
 }
 
@@ -103,5 +218,23 @@ const Label = ({ children, htmlFor }) => {
     >
       {children}
     </label>
+  );
+};
+
+const LabelWithBadge = ({ children, htmlFor, badge = 0 }) => {
+  const renderBadge = () => {
+    if (!badge) return null;
+    return (
+      <span className="dark:bg-dark-subtle bg-light-subtle text-white absolute top-0 right-0 translate-x-2 -translate-y-1 text-xs w-5 h-5 rounded-full flex justify-center items-center">
+        {badge <= 9 ? badge : "9+"}
+      </span>
+    );
+  };
+
+  return (
+    <div className="relative">
+      <Label htmlFor={htmlFor}>{children}</Label>
+      {renderBadge()}
+    </div>
   );
 };
